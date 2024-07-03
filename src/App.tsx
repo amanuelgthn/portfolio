fa3d324ffa6a495c97dd018581c5fb13619edc96
amanuelgthn <amanuelgthn@gmail.com>
2024-07-03 20:03:18 +0200

    update

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './components/Styling/header.css'
+import amanlogo  from '/amanlogo.svg'
+import Header from './components/ Header'
+import Home from './components/Home/Home'
+import About from './components/about/About'
+
+function App() {
+  
+  return (
+    <>
+    <Header />
+    <main className="main">
+      <Home />
+      <About />
+    </main>
+          <img className="image__logo"
+             src={amanlogo}
+             width={100}
+          />
+  </>
+);
+};
+
+
+export default App
---
+++ src/components/Home/Data.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+const Data = () => {
+
+  return (
+    <div className="home__data">
+        <h1 className="home__title">
+            Amanuel Getahun
+        </h1>
+        <span className='home__title'>Bikora</span>
+        <h3 className="home__subtitle">Software Engineer💻</h3>
+        <br />
+        <p className="home__description">As a recent full-stack software engineering boot camp graduate, my
+            proﬁciency in Python, C, JavaScript, TypeScript, and bash scripting is
+            complemented by my adeptness in Flask and React frameworks. My
+            familiarity with the Linux environment, MySQL database
+            management, and ORM technologies, especially SQLAlchemy, is
+            robust. Additionally, my ability to conﬁgure web servers with
+            HAProxy and Nginx underscores my technical versatility. My
+            problem-solving skills, gained in the manufacturing industry, enable
+            me to approach challenges with a unique perspective, setting me
+            apart in the tech ﬁeld.
+        </p>
+        <br />
+        <a href="#contact" className="button button--flex">
+            Stay in touch &nbsp;
+            <i className="uil-message"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Data
